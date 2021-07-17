4cc2e8866563291ee00cda8e25f3581f354d1597
daalre <77889030+daalre@users.noreply.github.com>
2021-07-17 21:53:36 +0430

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,14 +94,14 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
+    text = "<b>برای دسترسی باید وارد کانال بشی</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
+        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>سعی محدد</a></b>"
     except ValueError:
         pass
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ورود به کانال", url = client.invitelink)]])
     await message.reply(
         text = text,
         reply_markup = reply_markup,
